422af44556e71b1feaac07ce944a4d9c599db907
marykrasilova-rgb <marykrasilova@gmail.com>
2026-09-10 12:49:34 +0300

    Load layered plum voice waveform treatment

--- voice-cards.js
+++ voice-cards.js
@@ -1,4 +1,5 @@
 import './theme-test.js';
+import './deep-wave.js';
 import {createClient} from 'https://esm.sh/@supabase/supabase-js@2.57.4';
 
 const s=createClient('https://uecdlqlwsrqmocbpgiwj.supabase.co','sb_publishable_QJ_4e8-BHl0gOZifGqdv1w_doFwpTlb');
