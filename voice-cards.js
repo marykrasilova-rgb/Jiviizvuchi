df63c96862564371b065d43f86416150b6d9a1ca
marykrasilova-rgb <marykrasilova@gmail.com>
2026-09-10 12:44:11 +0300

    Load color theme tester in emotional diary

--- voice-cards.js
+++ voice-cards.js
@@ -1,3 +1,4 @@
+import './theme-test.js';
 import {createClient} from 'https://esm.sh/@supabase/supabase-js@2.57.4';
 
 const s=createClient('https://uecdlqlwsrqmocbpgiwj.supabase.co','sb_publishable_QJ_4e8-BHl0gOZifGqdv1w_doFwpTlb');
